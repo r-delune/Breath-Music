4e4007301e1e7fa3e7a196d43d5bdd7993b9ae83
Brian Dillon <briandillon@Brians-Mac-mini.local>
2018-09-27 10:57:07 +0100

    first commit

--- BreathMusic/btlemanager/BTLEManager.m
+++ BreathMusic/btlemanager/BTLEManager.m
@@ -95,13 +95,15 @@
     
     __unsafe_unretained BTLEManager *weakSelf = self;
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), _queue, ^{
+    
+    //ios12
+    //dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), _queue, ^{
         if (weakSelf) {
             [weakSelf startScanning];
             
         }
         
-    });
+    //});
     
     //self.pollTimer=[NSTimer scheduledTimerWithTimeInterval:interval target:self selector:@selector(requestBTData:) userInfo:nil repeats:YES];
     [self startTimerWithInterval:interval];
@@ -197,13 +199,11 @@
     
     self.melody = [MelodyManager foundDeviceAtIndex:0];
     self.melody.delegate = self;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), _queue, ^{
-        
+    
+    //ios12
+    //dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), _queue, ^{
         [self.melody connect];
-    });
-    
-    
-    
+    //});
 }
 #pragma mark -
 #pragma mark - LED ON OFF
